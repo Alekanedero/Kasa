c6226b5837e30e13449c68544670dfea406a8cdf
Alex <alexandrecompin@gmail.com>
2023-08-16 14:11:58 +0200

    création title, name, img

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -1,9 +1,9 @@
-import Carrousel from '../components/Carrousel';
 import jsonData from '../data/data.json';
 import { useParams } from 'react-router-dom';
+import Carrousel from '../components/Carrousel';
+import Tag from '../components/Tag.jsx';
 
 import '../style/house.css';
-import Tag from '../components/Tag.jsx';
 
 
 const House = () => {
@@ -11,8 +11,18 @@ const House = () => {
     const datas = jsonData.find((house) => house.id === id.id);
 
     return (
-        <div className="Home">
-            {/* <Carrousel images={datas.pictures} /> */}
+        <div className="house">
+            <Carrousel images={datas.pictures} />
+            <div className='info_container'>
+                <div>
+                    <h1 className='title'>{datas.title}</h1>
+                    <h2 className='location'>{datas.location}</h2>
+                </div>
+                <div className='name_img_container'>
+                    <p className='name'>{datas.host.name}</p>
+                    <img src={datas.host.picture} alt="photo de profil" className='img_profil'/>
+                </div>
+            </div>
             <Tag tags={datas.tags} />
         </div>
     );
