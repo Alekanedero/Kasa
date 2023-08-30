ba9d3d92e75cc700faac2eb7e261bc4614316462
Alex <alexandrecompin@gmail.com>
2023-08-30 13:13:27 +0200

    indentation

--- src/pages/House.jsx
+++ src/pages/House.jsx
@@ -10,52 +10,59 @@ function House() {
    const id = useParams()
    const datas = jsonData.find((house) => house.id === id.id)
 
-
-   return (  <> {
-
-      datas ? ( <div className="house">
-      <Slideshow
-         images={datas.pictures}
-         alt={datas.title}
-      />
-      <div className="container">
-         <div className="container__title__tag">
-            <div>
-               <h1 className="container__title">{datas.title}</h1>
-               <h2 className="container__location">{datas.location}</h2>
-            </div>
-            <div>
-               <Tag tags={datas.tags} />
-            </div>
-         </div>
-         <div className="container__name__rating">
-            <div className="container__name__img">
-               <p className="container__name">{datas.host.name}</p>
-               <img
-                  src={datas.host.picture}
-                  alt="profil"
-                  className="container__img-profil"
+   return (
+      <>
+         { datas ? (
+            <div className="house">
+               <Slideshow
+                  images={datas.pictures}
+                  alt={datas.title}
                />
+               <div className="container">
+                  <div className="container__title__tag">
+                     <div>
+                        <h1 className="container__title">{datas.title}</h1>
+                        <h2 className="container__location">
+                           {datas.location}
+                        </h2>
+                     </div>
+                     <div>
+                        <Tag tags={datas.tags} />
+                     </div>
+                  </div>
+                  <div className="container__name__rating">
+                     <div className="container__name__img">
+                        <p className="container__name">{datas.host.name}</p>
+                        <img
+                           src={datas.host.picture}
+                           alt="profil"
+                           className="container__img-profil"
+                        />
+                     </div>
+                     <div>
+                        <Rating rating={datas.rating} />
+                     </div>
+                  </div>
+               </div>
+               <div className="container-collapse">
+                  <Collapse
+                     title="Description"
+                     details={datas.description}
+                  />
+                  <Collapse
+                     title="Equipements"
+                     details={datas.equipments}
+                  />
+               </div>
             </div>
-            <div>
-               <Rating rating={datas.rating} />
-            </div>
-         </div>
-      </div>
-      <div className="container-collapse">
-         <Collapse
-            title="Description"
-            details={datas.description}
-         />
-         <Collapse
-            title="Equipements"
-            details={datas.equipments}
-         />
-      </div>
-   </div> ) : (
-      <Navigate replace to='/404' />
-   )
-   }</>
+         ) : (
+            <Navigate
+               replace
+               to="/404"
+            />
+            )
+         }
+      </>
    )
 }
 
